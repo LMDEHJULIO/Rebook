f6caac70353aa296e5d056adbb98d4cc4c7cc85c
LMDEHJULIO <juliodevelops@gmail.com>
2017-11-09 15:37:39 -0500

    Initial Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'Functional programming'},
+    { title: 'Learning Scala'},
+    { title: 'Node JS Compendium'},
+    { title: 'Javascript: The Good Parts'},
+  ]
+}
